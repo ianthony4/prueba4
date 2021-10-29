878df3abe7a6dceedb80dd714a25329278cdc3e8
Anthony <achaisa@unsa.edu.pe>
2021-10-29 14:18:09 +0000

    movemos el print ->header

--- cgi-bin/pcalculadora.pl
+++ cgi-bin/pcalculadora.pl
@@ -5,8 +5,9 @@ use CGI;
 
 #Recogemos Datos
 my $form = CGI->new;
-print $form->header('text/html');
 
 my $var1 = $form->param('variable1');
 my $oper = $form->param('operador');
 my $var2 = $form->param('variable2');
+
+print $form->header('text/html');
