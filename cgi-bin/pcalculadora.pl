83e17606bad0756b9b378c893e54429f44f565b4
Anthony <achaisa@unsa.edu.pe>
2021-10-29 14:01:54 +0000

    cambio variable num1, num2 por var1, var2 respectivamente

--- cgi-bin/pcalculadora.pl
+++ cgi-bin/pcalculadora.pl
@@ -5,6 +5,6 @@ use CGI;
 
 #Recogemos Datos
 my $form = new CGI;
-my $num1 = $form->param('variable1');
+my $var1 = $form->param('variable1');
 my $oper = $form->param('operador');
-my $num2 = $form->param('variable2');
+my $var2 = $form->param('variable2');
